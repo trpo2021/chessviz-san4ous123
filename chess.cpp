472157d72d84e0a5f5d35d0ad59c15cbe2d680ff
san4ous123 <sasha99928@mail.ru>
2021-02-26 13:02:19 +0300

    Create new code chess.cpp

---
+++ chess.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+ 
+cout<<"8 r n b q k b n r\n7 p p p p p p p p\n6\n5\n4\n3\n2 P P P P P P P P\n1 R N B Q K B N R\n  a b c d e f g h "<<endl;
+ 
+    return 0;
+}
