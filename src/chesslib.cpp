9130fa9d2070318d90cdb9a4fd6e5a361c65f350
san4ous123 <sasha99928@mail.ru>
2021-04-27 23:25:04 +0700

    Add init_desk function for initialize array of char

--- src/chesslib.cpp
+++ src/chesslib.cpp
@@ -1,1 +1,30 @@
+#include <chesslib.hpp>
 
+void init_desk(char* desk[8])
+{
+    desk[0][0] = 'r';
+    desk[0][1] = 'n';
+    desk[0][2] = 'b';
+    desk[0][3] = 'q';
+    desk[0][4] = 'k';
+    desk[0][5] = 'b';
+    desk[0][6] = 'n';
+    desk[0][7] = 'r';
+
+    for (int i = 0; i < 8; i++)
+        desk[1][i] = 'p';
+    for (int i = 2; i < 6; i++)
+        for (int j = 0; j < 8; j++)
+            desk[i][j] = ' ';
+    for (int i = 0; i < 8; i++)
+        desk[6][i] = 'P';
+
+    desk[7][0] = 'R';
+    desk[7][1] = 'N';
+    desk[7][2] = 'B';
+    desk[7][3] = 'Q';
+    desk[7][4] = 'K';
+    desk[7][5] = 'B';
+    desk[7][6] = 'N';
+    desk[7][7] = 'R';
+}
--- src/include/chesslib.hpp
+++ src/include/chesslib.hpp
@@ -1,1 +1,3 @@
+#pragma once
 
+void init_desk(char* desk[8]);
